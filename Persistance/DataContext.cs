5dd48d079a05e348ce2a74cca8584904bb53a4d6
Szymon Kozlowski <szymonex@vp.pl>
2020-01-06 21:44:09 +0100

    Initial Commit

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Seed();
+        }
+    }
+}
---
+++ Persistance/ModelBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistance
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value { Id = 1, Name = "Value 101" },
+                new Value { Id = 2, Name = "Value 102" },
+                new Value { Id = 3, Name = "Value 103" }
+                );
+        }
+    }
+}
